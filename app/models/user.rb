3951d4fe67f66d8564d9ed191dfc21f086a0a352
Feliperto <fperezferraro@gmail.com>
2023-11-01 16:00:10 -0300

    Added methods to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
   validates :user_name, presence: true
   validates :photo, presence: true
+
+  def get_posts
+    posts.order(created_at: :desc).limit(3)
+  end
 end
