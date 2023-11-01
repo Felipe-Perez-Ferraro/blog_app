e22f667083bf51fc71a96d7f343fdefbe52cb73d
Feliperto <fperezferraro@gmail.com>
2023-11-01 16:23:15 -0300

    Solved errors as my code reviewer told me

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
-  validates :user_name, presence: true
+  validates :name, presence: true
   validates :photo, presence: true
+
+  has_many :posts 
+  has_many :comments
+  has_many :likes
 
   def order_user_posts
     posts.order(created_at: :desc).limit(3)
