8d5965ea4bfb1f6a9748ac19c4c56d5a2e48f191
Feliperto <fperezferraro@gmail.com>
2023-11-01 16:00:17 -0300

    Added methods to post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,15 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
+
+  after_save: :post_counter
+
+  def get_comments
+    comments.order(created_at: :desc).limit(5)
+  end
+
+  private
+
+  def post_counter
+    author.update(post_counter: author.posts.count)
+  end
 end
