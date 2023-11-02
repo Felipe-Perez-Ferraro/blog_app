7ef2878bdd6f06b66c7039983d68b042d3a1b3d9
Feliperto <fperezferraro@gmail.com>
2023-11-02 14:15:39 -0300

    Fixed linter errors

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  let (:user) { User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.') }
+  let(:user) { User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.') }
 
-  let (:post) { Post.new(author: user, title: 'A title', text: 'This is my first post') }
+  let(:post) { Post.new(author: user, title: 'A title', text: 'This is my first post') }
 
   it 'Should be valid' do
     expect(post).to be_valid
@@ -28,19 +28,19 @@ RSpec.describe Post, type: :model do
   end
 
   it 'Should bring the 5 first comments' do
-    first_com = Comment.create(post: post, user: user, text: 'Hi Tom!')
-    second_com = Comment.create(post: post, user: user, text: 'Hi Tom!')
-    third_com = Comment.create(post: post, user: user, text: 'Hi Tom!')
-    fourth_com = Comment.create(post: post, user: user, text: 'Hi Tom!')
-    fifth_com = Comment.create(post: post, user: user, text: 'Hi Tom!')
-    sixth_com = Comment.create(post: post, user: user, text: 'Hi Tom!')
+    first_com = Comment.create(post:, user:, text: 'Hi Tom!')
+    second_com = Comment.create(post:, user:, text: 'Hi Tom!')
+    third_com = Comment.create(post:, user:, text: 'Hi Tom!')
+    fourth_com = Comment.create(post:, user:, text: 'Hi Tom!')
+    fifth_com = Comment.create(post:, user:, text: 'Hi Tom!')
+    sixth_com = Comment.create(post:, user:, text: 'Hi Tom!')
 
     expect(post.comments_counter).to eq(6)
     expect(post.order_post_comments).to eq([sixth_com, fifth_com, fourth_com, third_com, second_com])
   end
 
-  it 'Should have 1 like in the post' do 
-    like = Like.create(post: post, user: user)
+  it 'Should have 1 like in the post' do
+    like = Like.create(post:, user:)
     expect(post.likes_counter).to eq(1)
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,22 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let (:user) { User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.') }
-  
+  let(:user) { User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.') }
+
   it 'Should be valid' do
     expect(user).to be_valid
   end
-  
+
   it 'Should not be valid (name example)' do
     user.name = nil
     expect(user).to_not be_valid
   end
-  
+
   it 'Should not be valid (photo example)' do
     user.photo = nil
     expect(user).to_not be_valid
   end
-  
+
   it 'Should have 0 posts' do
     expect(user.post_counter).to eq(0)
   end
@@ -29,5 +29,5 @@ RSpec.describe User, type: :model do
 
     expect(user.post_counter).to eq(4)
     expect(user.order_user_posts).to eq([fourth_post, third_post, second_post])
-  end 
+  end
 end
