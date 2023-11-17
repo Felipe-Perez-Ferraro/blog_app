0eae155a28b58a2f97feb45b0f5fd7dd5ea843b2
Felipe <fperezferraro@gmail.com>
2023-11-17 12:17:50 -0300

    Generated the ability file and added authorizations

---
+++ app/models/ability.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    user ||= User.new
+
+    if user.admin?
+      can :manage, :all
+    else
+      can :delete, Post, author_id: user.id
+      can :delete, Comment, user_id: user.id
+      can :read, Post, author_id: user.id
+    end
+  end
+end
