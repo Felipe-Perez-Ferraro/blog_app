b09a5b1df9def830f1f3ee3c9335db689abddf89
sergiok <sergioandresusma@hotmail.com>
2023-11-17 11:05:55 -0500

    Update user model to add authorizations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,18 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable
   validates :name, presence: true
   validates :bio, presence: true
   validates :post_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   has_many :posts, foreign_key: 'author_id'
-  has_many :comments
-  has_many :likes
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
+
+  def admin?
+    role == 'admin'
+  end
 
   def order_user_posts
     posts.order(created_at: :desc).limit(3)
